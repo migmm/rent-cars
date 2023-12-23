6fe57565c4ec76d42cfcd4764550ec224ede7387
Miguel <miguelm@hotmail.com.ar>
2023-12-23 10:56:37 -0300

    Moved js code into a separated file

--- views/cars/createCar.php
+++ views/cars/createCar.php
@@ -132,35 +132,7 @@ include('../configs/database.php');
     </form>
     <a href="cars.php">Back to list</a>
 
-    <script>
-
-        async function getCities() {
-            var countrySelect = document.getElementById("countrySelect");
-            var citySelect = document.getElementById("citySelect");
-
-            var selectedCountry = countrySelect.value;
-
-            try {
-                var response = await fetch(`utils/getCities.php?country_id=${selectedCountry}`);
-                var cities = await response.json();
-
-                citySelect.innerHTML = "";
-                for (var i = 0; i < cities.length; i++) {
-                    var option = document.createElement("option");
-                    option.value = cities[i].id;
-                    option.text = cities[i].name;
-                    citySelect.add(option);
-                }
-            } catch (error) {
-                console.error("Error al obtener ciudades:", error);
-            }
-        }
-
-        document.addEventListener("DOMContentLoaded", function() {
-            getCities();
-        });
-
-    </script>
+    <script src="js/main.js"></script>
 </body>
 
 </html>
