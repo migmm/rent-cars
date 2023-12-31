c0d2c40d8ca92d7df8c1ec42f7519df2386598ad
Miguel <miguelm@hotmail.com.ar>
2023-12-31 00:44:19 -0300

    Added image preview on edit

--- public/js/main.js
+++ public/js/main.js
@@ -20,6 +20,23 @@ async function getCities() {
     }
 }
 
+function previewProfilePicture(event) {
+    const input = event.target;
+    const preview = document.getElementById('profilePicturePreview');
+
+    const file = input.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            preview.src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = '<?php echo $profile_picture; ?>';
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     getCities();
 });
+
