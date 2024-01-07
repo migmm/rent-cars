86246f9440a138eb5b3886007124befaed0899b4
Miguel <miguelm@hotmail.com.ar>
2024-01-07 01:37:11 -0300

    Added new logo and separate files

--- views/partials/header/header.php
+++ views/partials/header/header.php
@@ -1,3 +1,21 @@
 <header class="header">
+    <a href="#" class="logo">
+        <img src="images/logo.png" alt="">
+    </a>
+
+    <input class="side-menu" type="checkbox" id="side-menu" />
+    <label class="hamburguer-button" for="side-menu"><span class="hamburguer-button-line"></span></label>
+
+    <nav class="nav">
+        <ul class="menu">
+            <li><a href="#">Reservations</a></li>
+            <li><a href="#">Checkins</a> </li>
+            <li><a href="#">Locations</a> </li>
+            <li><a href="#">Vehicles</a></li>
+        </ul>
+        <a href="#" class="menu">
+        <div class="sign-in-button-nv">Sign in</div>
+    </a>
+    </nav>
 
 </header>
