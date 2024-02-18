1b35263b62960f73a6ef06a247740ba65b693f74
Miguel <miguelm@hotmail.com.ar>
2024-02-17 23:12:18 -0300

    Fixed error in image when does not exist

--- controllers/CarController.php
+++ controllers/CarController.php
@@ -156,22 +156,31 @@ class RentalCarController
     }
 
     function resizeAndSaveImage($sourcePath, $targetPath, $newWidth, $newHeight) {
+        if (!file_exists($sourcePath)) {
+            die("Source image doesn't exist.");
+        }
+    
         list($originalWidth, $originalHeight) = getimagesize($sourcePath);
-
+    
+        // Check if image dimensions are valid
+        if ($originalWidth == 0 || $originalHeight == 0) {
+            die("Invalid image dimensions.");
+        }
+    
         $ratio = $originalWidth / $originalHeight;
         if ($newWidth / $newHeight > $ratio) {
             $newWidth = $newHeight * $ratio;
         } else {
             $newHeight = $newWidth / $ratio;
         }
-
+    
         $sourceImage = imagecreatefromjpeg($sourcePath);
-
+    
         $newWidth = round($newWidth);
         $newHeight = round($newHeight);
-
+    
         $resizedImage = imagecreatetruecolor($newWidth, $newHeight);
-
+    
         imagecopyresampled(
             $resizedImage,
             $sourceImage,
@@ -184,12 +193,13 @@ class RentalCarController
             $originalWidth,
             $originalHeight
         );
-
+    
         imagejpeg($resizedImage, $targetPath);
-
+    
         imagedestroy($sourceImage);
         imagedestroy($resizedImage);
     }
+    
 
     public function deleteCar($carId)
     {
