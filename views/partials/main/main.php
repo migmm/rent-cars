63e3c8283f37244b3c4f4af1ee140a78c8b6388d
Miguel <micmigm@gmail.com>
2024-05-09 21:45:55 -0300

    Fixed html

--- views/partials/main/main.php
+++ views/partials/main/main.php
@@ -1,202 +1,161 @@
-<main>
-
-    <section>
-        <h1>Rent a car and drive happy with our country coverage</h1>
-        <div>
-            <div>
-                <h2>
-                    Start a reservation
-                </h2>
-            </div>
-            <div>
-                <input type="select">
-            </div>
-            <div>
-                <input type="date">
-                <input type="select">
-                <input type="date">
-                <input type="submit">
-            </div>
-            <div>
-                * You can do rent a car without register
-            </div>
-        </div>
-    </section>
     <main>
-    <section>
-        <div>
-            <select>
-                <option value="1" selected>Return in same place</option>
-                <option value="2">Return in a different place</option>
-            </select>
-            <div>Cancel</div>
-        </div>
-        <div>
-            <label for="city">Pick-up city</label>
-            <input type="text" name="city" id="city">
-        </div>
-        <div>
-            <div>
-                <label for="pickup-date">Pick-up date</label>
-                <input type="date" name="pickup-date" id="pickup-date">
-            </div>
-            <div>
-                <label for="pickup-time">Pick-up time</label>
-                <select name="pickup-time" id="pickup-time">
-                    <option value="00:00">00:00</option>
-                    <option value="00:30">00:30</option>
-                    <option value="01:00">01:00</option>
-                    <option value="01:30">01:30</option>
-                    <option value="02:00">02:00</option>
-                    <option value="02:30">02:30</option>
-                    <option value="03:00">03:00</option>
-                    <option value="03:30">03:30</option>
-                    <option value="04:00">04:00</option>
-                    <option value="04:30">04:30</option>
-                    <option value="05:00">05:00</option>
-                    <option value="05:30">05:30</option>
-                    <option value="06:00">06:00</option>
-                    <option value="06:30">06:30</option>
-                    <option value="07:00">07:00</option>
-                    <option value="07:30">07:30</option>
-                    <option value="08:00">08:00</option>
-                    <option value="08:30">08:30</option>
-                    <option value="09:00">09:00</option>
-                    <option value="09:30">09:30</option>
-                    <option value="10:00">10:00</option>
-                    <option value="10:30">10:30</option>
-                    <option value="11:00">11:00</option>
-                    <option value="11:30">11:30</option>
-                    <option value="12:00">12:00</option>
-                    <option value="12:30">12:30</option>
-                    <option value="13:00">13:00</option>
-                    <option value="13:30">13:30</option>
-                    <option value="14:00">14:00</option>
-                    <option value="14:30">14:30</option>
-                    <option value="15:00">15:00</option>
-                    <option value="15:30">15:30</option>
-                    <option value="16:00">16:00</option>
-                    <option value="16:30">16:30</option>
-                    <option value="17:00">17:00</option>
-                    <option value="17:30">17:30</option>
-                    <option value="18:00">18:00</option>
-                    <option value="18:30">18:30</option>
-                    <option value="19:00">19:00</option>
-                    <option value="19:30">19:30</option>
-                    <option value="20:00">20:00</option>
-                    <option value="20:30">20:30</option>
-                    <option value="21:00">21:00</option>
-                    <option value="21:30">21:30</option>
-                    <option value="22:00">22:00</option>
-                    <option value="22:30">22:30</option>
-                    <option value="23:00">23:00</option>
-                    <option value="23:30">23:30</option>
+        <section class="island-container">
+            <div class="island-top-menu">
+                <select>
+                    <option value="1" selected>Return in same place</option>
+                    <option value="2">Return in a different place</option>
                 </select>
+                <div>Cancel</div>
             </div>
-        </div>
-        <div>
+            <div class="island-selector-menu">
+                <div class="island-options">
+                    <label for="city">Pick-up city</label>
+                    <input type="text" name="city" id="city">
+                </div>
+                <div class="island-options">
+                    <label for="pickup-date">Pick-up date</label>
+                    <input type="date" name="pickup-date" id="pickup-date">
+                </div>
+                <div class="island-options">
+                    <label for="pickup-time">Pick-up time</label>
+                    <select name="pickup-time" id="pickup-time">
+                        <option value="00:00">00:00</option>
+                        <option value="00:30">00:30</option>
+                        <option value="01:00">01:00</option>
+                        <option value="01:30">01:30</option>
+                        <option value="02:00">02:00</option>
+                        <option value="02:30">02:30</option>
+                        <option value="03:00">03:00</option>
+                        <option value="03:30">03:30</option>
+                        <option value="04:00">04:00</option>
+                        <option value="04:30">04:30</option>
+                        <option value="05:00">05:00</option>
+                        <option value="05:30">05:30</option>
+                        <option value="06:00">06:00</option>
+                        <option value="06:30">06:30</option>
+                        <option value="07:00">07:00</option>
+                        <option value="07:30">07:30</option>
+                        <option value="08:00">08:00</option>
+                        <option value="08:30">08:30</option>
+                        <option value="09:00">09:00</option>
+                        <option value="09:30">09:30</option>
+                        <option value="10:00">10:00</option>
+                        <option value="10:30">10:30</option>
+                        <option value="11:00">11:00</option>
+                        <option value="11:30">11:30</option>
+                        <option value="12:00">12:00</option>
+                        <option value="12:30">12:30</option>
+                        <option value="13:00">13:00</option>
+                        <option value="13:30">13:30</option>
+                        <option value="14:00">14:00</option>
+                        <option value="14:30">14:30</option>
+                        <option value="15:00">15:00</option>
+                        <option value="15:30">15:30</option>
+                        <option value="16:00">16:00</option>
+                        <option value="16:30">16:30</option>
+                        <option value="17:00">17:00</option>
+                        <option value="17:30">17:30</option>
+                        <option value="18:00">18:00</option>
+                        <option value="18:30">18:30</option>
+                        <option value="19:00">19:00</option>
+                        <option value="19:30">19:30</option>
+                        <option value="20:00">20:00</option>
+                        <option value="20:30">20:30</option>
+                        <option value="21:00">21:00</option>
+                        <option value="21:30">21:30</option>
+                        <option value="22:00">22:00</option>
+                        <option value="22:30">22:30</option>
+                        <option value="23:00">23:00</option>
+                        <option value="23:30">23:30</option>
+                    </select>
+                </div>
+                <div class="island-options">
+                    <label for="dropoff-date">Drop-off date</label>
+                    <input type="date" name="dropoff-date" id="dropoff-date">
+                </div>
+                <div class="island-options">
+                    <label for="dropoff-time">Drop-off time</label>
+                    <select name="dropoff-time" id="dropoff-time">
+                        <option value="00:00">00:00</option>
+                        <option value="00:30">00:30</option>
+                        <option value="01:00">01:00</option>
+                        <option value="01:30">01:30</option>
+                        <option value="02:00">02:00</option>
+                        <option value="02:30">02:30</option>
+                        <option value="03:00">03:00</option>
+                        <option value="03:30">03:30</option>
+                        <option value="04:00">04:00</option>
+                        <option value="04:30">04:30</option>
+                        <option value="05:00">05:00</option>
+                        <option value="05:30">05:30</option>
+                        <option value="06:00">06:00</option>
+                        <option value="06:30">06:30</option>
+                        <option value="07:00">07:00</option>
+                        <option value="07:30">07:30</option>
+                        <option value="08:00">08:00</option>
+                        <option value="08:30">08:30</option>
+                        <option value="09:00">09:00</option>
+                        <option value="09:30">09:30</option>
+                        <option value="10:00">10:00</option>
+                        <option value="10:30">10:30</option>
+                        <option value="11:00">11:00</option>
+                        <option value="11:30">11:30</option>
+                        <option value="12:00">12:00</option>
+                        <option value="12:30">12:30</option>
+                        <option value="13:00">13:00</option>
+                        <option value="13:30">13:30</option>
+                        <option value="14:00">14:00</option>
+                        <option value="14:30">14:30</option>
+                        <option value="15:00">15:00</option>
+                        <option value="15:30">15:30</option>
+                        <option value="16:00">16:00</option>
+                        <option value="16:30">16:30</option>
+                        <option value="17:00">17:00</option>
+                        <option value="17:30">17:30</option>
+                        <option value="18:00">18:00</option>
+                        <option value="18:30">18:30</option>
+                        <option value="19:00">19:00</option>
+                        <option value="19:30">19:30</option>
+                        <option value="20:00">20:00</option>
+                        <option value="20:30">20:30</option>
+                        <option value="21:00">21:00</option>
+                        <option value="21:30">21:30</option>
+                        <option value="22:00">22:00</option>
+                        <option value="22:30">22:30</option>
+                        <option value="23:00">23:00</option>
+                        <option value="23:30">23:30</option>
+                    </select>
+                </div>
+                <button>Continue</button>
+            </div>
+            
+        </section>
+        <section>
             <div>
-                <label for="dropoff-date">Drop-off date</label>
-                <input type="date" name="dropoff-date" id="dropoff-date">
+                <h1>Rental car fleet around the world</h1>
+                <p> We have a diverse fleet of rental vehicles around the world for you to choose from, ensuring you find the one that best suits your needs and preferences. We offer quality and safety at the best price.</p>
             </div>
             <div>
-                <label for="dropoff-time">Drop-off time</label>
-                <select name="dropoff-time" id="dropoff-time">
-                    <option value="00:00">00:00</option>
-                    <option value="00:30">00:30</option>
-                    <option value="01:00">01:00</option>
-                    <option value="01:30">01:30</option>
-                    <option value="02:00">02:00</option>
-                    <option value="02:30">02:30</option>
-                    <option value="03:00">03:00</option>
-                    <option value="03:30">03:30</option>
-                    <option value="04:00">04:00</option>
-                    <option value="04:30">04:30</option>
-                    <option value="05:00">05:00</option>
-                    <option value="05:30">05:30</option>
-                    <option value="06:00">06:00</option>
-                    <option value="06:30">06:30</option>
-                    <option value="07:00">07:00</option>
-                    <option value="07:30">07:30</option>
-                    <option value="08:00">08:00</option>
-                    <option value="08:30">08:30</option>
-                    <option value="09:00">09:00</option>
-                    <option value="09:30">09:30</option>
-                    <option value="10:00">10:00</option>
-                    <option value="10:30">10:30</option>
-                    <option value="11:00">11:00</option>
-                    <option value="11:30">11:30</option>
-                    <option value="12:00">12:00</option>
-                    <option value="12:30">12:30</option>
-                    <option value="13:00">13:00</option>
-                    <option value="13:30">13:30</option>
-                    <option value="14:00">14:00</option>
-                    <option value="14:30">14:30</option>
-                    <option value="15:00">15:00</option>
-                    <option value="15:30">15:30</option>
-                    <option value="16:00">16:00</option>
-                    <option value="16:30">16:30</option>
-                    <option value="17:00">17:00</option>
-                    <option value="17:30">17:30</option>
-                    <option value="18:00">18:00</option>
-                    <option value="18:30">18:30</option>
-                    <option value="19:00">19:00</option>
-                    <option value="19:30">19:30</option>
-                    <option value="20:00">20:00</option>
-                    <option value="20:30">20:30</option>
-                    <option value="21:00">21:00</option>
-                    <option value="21:30">21:30</option>
-                    <option value="22:00">22:00</option>
-                    <option value="22:30">22:30</option>
-                    <option value="23:00">23:00</option>
-                    <option value="23:30">23:30</option>
-                </select>
+                <div class="car-card"></div>
+                <div class="car-card"></div>
+                <div class="car-card"></div>
             </div>
-        </div>
-        <button>Continue</button>
-    </section>
-    <section>
-        <div>
-            <h1>Rental car fleet around the world</h1>
-            <p> We have a diverse fleet of rental vehicles around the world for you to choose from, ensuring you find the one that best suits your needs and preferences. We offer quality and safety at the best price.</p>
-        </div>
-        <div>
-            <div id="car-card"></div>
-            <div id="car-card"></div>
-            <div id="car-card"></div>
-        </div>
-        <button>View entire fleet</button>
-    </section>
-    <section>
-        <h2>Frequently asked questions about car rental</h2>
-        <div>
-            <div>What is the price of car rental?</div>
-            <div>The minimum price to rent a car may vary depending on the type of vehicle and additional services requested. We offer a wide selection of vehicles, from economical options to high-end luxury car rental services. You can check our complete fleet of rental cars here.</div>
+            <button>View entire fleet</button>
+        </section>
+        <section>
+            <h2>Frequently asked questions about car rental</h2>
+            <div>
+                <div>What is the price of car rental?</div>
+                <div>The minimum price to rent a car may vary depending on the type of vehicle and additional services requested. We offer a wide selection of vehicles, from economical options to high-end luxury car rental services. You can check our complete fleet of rental cars here.</div>
 
-            <div>How can I find rental cars near me?</div>
-            <div>We have multiple car rental depots around the world. To find the nearest car rental branch, you can click on the link above.</div>
+                <div>How can I find rental cars near me?</div>
+                <div>We have multiple car rental depots around the world. To find the nearest car rental branch, you can click on the link above.</div>
 
-            <div>What does the car rental service include?</div>
-            <div>The car rental service includes basic coverage for driving, 24/7 roadside assistance, theft and third-party damage insurance, and usually unlimited mileage. Additional services such as baby seats, GPS, and extra drivers are also available.</div>
+                <div>What does the car rental service include?</div>
+                <div>The car rental service includes basic coverage for driving, 24/7 roadside assistance, theft and third-party damage insurance, and usually unlimited mileage. Additional services such as baby seats, GPS, and extra drivers are also available.</div>
 
-            <div>Can I rent a car for just one day? Are there any minimum rental periods?</div>
-            <div>Yes, it is possible to rent a car for just one day, although the rental rates may vary depending on the duration and type of vehicle you choose.</div>
-        </div>
-    </section>
-</main>
-
-    
-    <h1>Select an option:</h1>
-
-    <a href="cars.php">
-        <button>CarRouter</button>
-    </a>
-
-    <a href="users.php">
-        <button>UserRouter</button>
-    </a>
-
-    <a href="auth.php">
-        <button>LoginRouter</button>
-    </a>
-</main>
+                <div>Can I rent a car for just one day? Are there any minimum rental periods?</div>
+                <div>Yes, it is possible to rent a car for just one day, although the rental rates may vary depending on the duration and type of vehicle you choose.</div>
+            </div>
+        </section>
+    </main>
